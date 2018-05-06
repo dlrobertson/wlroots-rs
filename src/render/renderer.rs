080e73dc49a503ca50fa00c9094cb51ae90f6a8c
Timidger <APragmaticPlace@gmail.com>
2018-05-06 09:00:48 -0400

    Added render_scissor wrapper

--- src/render/renderer.rs
+++ src/render/renderer.rs
@@ -1,16 +1,17 @@
 //! TODO Documentation
 
+use std::ptr;
 use std::time::Duration;
 
 use libc::{c_float, c_int, c_void};
 
 use {Area, Output, PixmanRegion};
 use render::Texture;
-use wlroots_sys::{wl_shm_format, wlr_backend, wlr_backend_get_renderer,
+use wlroots_sys::{wl_shm_format, wlr_backend, wlr_backend_get_renderer, wlr_box,
                   wlr_render_ellipse_with_matrix, wlr_render_quad_with_matrix, wlr_render_rect,
                   wlr_render_texture, wlr_render_texture_with_matrix, wlr_renderer,
                   wlr_renderer_begin, wlr_renderer_clear, wlr_renderer_destroy, wlr_renderer_end,
-                  wlr_texture_from_pixels};
+                  wlr_renderer_scissor, wlr_texture_from_pixels};
 
 /// A generic interface for rendering to the screen.
 ///
@@ -167,6 +168,21 @@ impl<'output> Renderer<'output> {
     /// Renders a solid rectangle in the specified color.
     pub fn render_colored_rect(&mut self, area: Area, color: [f32; 4], matrix: [f32; 9]) {
         unsafe { wlr_render_rect(self.renderer, &area.into(), color.as_ptr(), matrix.as_ptr()) }
+    }
+
+    /// Defines a scissor box. Only pixels that lie within the scissor box can be
+    /// modified by drawing functions.
+    ///
+    /// Providing `None` for `area` to disable the scissor box.
+    pub fn render_scissor<A>(&mut self, area: A)
+        where A: Into<Option<Area>>
+    {
+        let mut area: Option<wlr_box> = area.into().map(|area| area.into());
+        let area_ptr = match area.as_mut() {
+            Some(area) => area as _,
+            None => ptr::null_mut()
+        };
+        unsafe { wlr_renderer_scissor(self.renderer, area_ptr) }
     }
 }
 
